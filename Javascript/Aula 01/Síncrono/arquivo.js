43526ed849d476aef708d62fe9b188e349fa02ab
Fertilesoil <gasnbloop@gmail.com>
2023-10-19 10:37:57 -0300

    Javascript

---
+++ Javascript/Aula 01/Síncrono/arquivo.js
@@ -0,0 +1,26 @@
+﻿let valor = 3 
+let valor2 = "3"
+
+console.log(valor)
+console.log(typeof (valor))
+console.log(typeof (valor2))
+
+
+
+function ola() {
+    console.log("Olá Mundo !!")
+}
+ola()
+
+function soma(numero1, numero2) {
+    return numero1 + numero2
+}
+console.log(soma(7, 15))
+
+// Arrow Functions
+
+const olaMundo = () => console.log("Olá Mundo, que interessante !!")
+olaMundo()
+
+const somaNum = (num1, num2) => num1 + num2
+console.log(somaNum(30, 76))
