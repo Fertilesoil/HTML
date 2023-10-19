b87ccdf25e429512dfb5a91a9b9cf360b4cea142
Fertilesoil <gasnbloop@gmail.com>
2023-10-19 12:01:35 -0300

    Sessão Síncrona

---
+++ Javascript/Aula 01/Síncrono/JS/script.js
@@ -0,0 +1,14 @@
+﻿let nome = document.getElementById('nome');
+let email = document.getElementById('email');
+let assunto = document.querySelector('#assunto');
+
+function olaMundo() {
+    alert("Olá Mundo !!")
+}
+
+function verificarCampos() {
+    if (nome.value == "" || email.value == "" || assunto.value == "") {
+        alert('Os campos são obrigatórios !')
+    }
+    else { alert('Os campos foram validados com sucesso !') }
+}
